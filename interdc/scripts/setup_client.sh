f6ea498952bc1df34f19073169be3dd5e1638553
cima <cima@zhaw.ch>
2015-08-31 11:46:52 +0200

    added scripts setting up basic OpenVPN based interDC connectivity

---
+++ interdc/scripts/setup_client.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Script for setting up OpenVPN client
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A INPUT -i tun+ -j ACCEPT
+iptables -A FORWARD -i tun+ -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth0 -o tun+ -j ACCEPT
+
+sudo apt-get update
+sudo apt-get install -y openvpn
+
+cp ./static.key /etc/openvpn/
+
+sed -e 's/SERVER_ADDRESS/'$SERVER_ADDRESS'/g' \
+    -e 's/SERVER_SUBNET/'$SERVER_SUBNET'/g' \
+    -e 's/SERVER_MASK/'$SERVER_SUBNET_MASK'/g' \
+    < ./client.conf.template > ./client.conf
+
+cp ./client.conf /etc/openpvpn/
+
+mkdir /etc/openvpn/ccd
+touch /etc/openvpn/ccd/client
+
+# TO BE CUSTOMIZED
+echo 'iroute '$CLIENT_SUBNET' '$CLIENT_MASK >> /etc/openvpn/ccd/client
+
+service openvpn start
---
+++ interdc/scripts/setup_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script for setting up OpenVPN server
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A INPUT -i tun+ -j ACCEPT
+iptables -A FORWARD -i tun+ -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth0 -o tun+ -j ACCEPT
+
+sudo apt-get update
+sudo apt-get install -y openvpn
+
+cp ./static.key /etc/openvpn/
+
+# Customize server config template
+sed -e 's/SERVER_SUBNET/'$SERVER_SUBNET'/g' \
+    -e 's/SERVER_MASK/'$SERVER_SUBNET_MASK'/g' \
+    -e 's/CLIENT_SUBNET/'$CLIENT_SUBNET'/g' \
+    -e 's/CLIENT_MASK/'$CLIENT_SUBNET_MASK'/g' \
+    < ./server.conf.template > ./server.conf
+
+cp ./server.conf /etc/openpvpn/
+
+service openvpn start

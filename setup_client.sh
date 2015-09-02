fa1bcaacb9aaa748c3ba09a653eaf08b05f4ad99
cima <cima@zhaw.ch>
2015-09-02 15:48:23 +0200

    fixed hardcoded paths

--- setup_client.sh
+++ setup_client.sh
@@ -11,14 +11,14 @@ iptables -A FORWARD -i eth0 -o tun+ -j ACCEPT
 sudo apt-get update
 sudo apt-get install -y openvpn
 
-cp ./static.key /etc/openvpn/
+cp /tmp/acen-interdc-master/static.key /etc/openvpn/
 
 sed -e 's/SERVER_ADDRESS/'$SERVER_ADDRESS'/g' \
     -e 's/SERVER_SUBNET/'$SERVER_SUBNET'/g' \
     -e 's/SERVER_MASK/'$SERVER_MASK'/g' \
-    < ./client.conf.template > ./client.conf
+    < /tmp/acen-interdc-master/client.conf.template > /tmp/acen-interdc-master/client.conf
 
-cp ./client.conf /etc/openvpn/
+cp /tmp/acen-interdc-master/client.conf /etc/openvpn/
 
 mkdir /etc/openvpn/ccd
 touch /etc/openvpn/ccd/client
--- setup_server.sh
+++ setup_server.sh
@@ -11,15 +11,15 @@ iptables -A FORWARD -i eth0 -o tun+ -j ACCEPT
 sudo apt-get update
 sudo apt-get install -y openvpn
 
-cp ./static.key /etc/openvpn/
+cp /tmp/acen-interdc-master/static.key /etc/openvpn/
 
 # Customize server config template
 sed -e 's/SERVER_SUBNET/'$SERVER_SUBNET'/g' \
     -e 's/SERVER_MASK/'$SERVER_MASK'/g' \
     -e 's/CLIENT_SUBNET/'$CLIENT_SUBNET'/g' \
     -e 's/CLIENT_MASK/'$CLIENT_MASK'/g' \
-    < ./server.conf.template > ./server.conf
+    < /tmp/acen-interdc-master/server.conf.template > /tmp/acen-interdc-master/server.conf
 
-cp ./server.conf /etc/openvpn/
+cp /tmp/acen-interdc-master/server.conf /etc/openvpn/
 
 service openvpn restart

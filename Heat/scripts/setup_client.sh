14e29172bdf64278aecc5e84cad763182a79e1a2
cima <cima@zhaw.ch>
2016-01-05 14:57:39 +0100

    added Heat template orchestrating OpenVPN tunnel between two isolated CloudStack networks from different VPCs

---
+++ Heat/scripts/setup_client.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "$sshkey" >> ~/.ssh/authorized_keys
+touch /tmp/vars
+echo SERVER_ADDRESS=$vpn_server_addr >> /tmp/vars
+echo SERVER_SUBNET=$server_subnet >> /tmp/vars
+echo SERVER_MASK=$server_mask >> /tmp/vars
+echo CLIENT_SUBNET=$client_subnet >> /tmp/vars
+echo CLIENT_MASK=$client_mask >> /tmp/vars
+wget https://github.com/icclab/acen-interdc/archive/master.tar.gz -O - | tar -xz --directory=/tmp/
+chmod +x /tmp/acen-interdc-master/setup_client.sh
+/tmp/acen-interdc-master/setup_client.sh

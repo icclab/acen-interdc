44a76d18c803a4579dc8020b1bd8be6b4bade347
cima <cima@zhaw.ch>
2016-05-04 09:47:17 +0200

    [src] added setup.py

---
+++ Hurtle/sm-so-bundle/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='interdc_so',
+      version='0.1',
+      description='ACeN InterDC SO',
+      author='ZHAW, Citrix',
+      author_email='cima@zhaw.ch',
+      url='blog.zhaw.ch/icclab',
+      license='Apache 2.0',
+      packages=['wsgi', 'mcn_cc_sdk'])
